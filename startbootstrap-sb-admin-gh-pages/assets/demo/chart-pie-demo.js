7abcf8cb0f8779b84b277b7a400dac418e19690e
Valen754 <hllzrd@gmail.com>
2025-01-08 02:00:11 +0700

    sisa profil page

--- startbootstrap-sb-admin-gh-pages/assets/demo/chart-pie-demo.js
+++ startbootstrap-sb-admin-gh-pages/assets/demo/chart-pie-demo.js
@@ -7,9 +7,9 @@ var ctx = document.getElementById("myPieChart");
 var myPieChart = new Chart(ctx, {
   type: 'pie',
   data: {
-    labels: ["Blue", "Red", "Yellow", "Green"],
+    labels: ["Coffee", "Non Coffee", "Food Snack", "Cemilan"],
     datasets: [{
-      data: [12.21, 15.58, 11.25, 8.32],
+      data: [20, 40, 20, 20],
       backgroundColor: ['#007bff', '#dc3545', '#ffc107', '#28a745'],
     }],
   },
